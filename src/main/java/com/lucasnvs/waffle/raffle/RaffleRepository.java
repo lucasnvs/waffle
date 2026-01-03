ce3f326e0193c6bf4d650167ed621e62519998d4
lucasnvs <lucasli.nev@hotmail.com>
2026-01-03 18:36:42 -0300

    feat(repository): Add @Repository annotation to RaffleRepository and TicketRepository

--- src/main/java/com/lucasnvs/waffle/raffle/RaffleRepository.java
+++ src/main/java/com/lucasnvs/waffle/raffle/RaffleRepository.java
@@ -1,6 +1,8 @@
 package com.lucasnvs.waffle.raffle;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface RaffleRepository extends JpaRepository<RaffleEntity, Long> {
 }
--- src/main/java/com/lucasnvs/waffle/ticket/TicketRepository.java
+++ src/main/java/com/lucasnvs/waffle/ticket/TicketRepository.java
@@ -1,7 +1,9 @@
 package com.lucasnvs.waffle.ticket;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface TicketRepository extends JpaRepository<TicketEntity, Long> {
 
     boolean existsByRaffleIdAndNumber(Long raffleId, Integer number);
